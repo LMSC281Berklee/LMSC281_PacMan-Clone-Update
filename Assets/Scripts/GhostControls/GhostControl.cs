329717654e2ddb0cc9751b08f9a0e38389c58f60
jcowen-Berklee <jcowen@berklee.edu>
2017-05-01 22:08:15 -0400

    Revert "Merge pull request #9 from LMSC281Berklee/PacMan_arivera"
    This reverts commit 78ef2ecc96215c7fa04787d5b9c45cd0a975c3fe, reversing
    changes made to 9f4165d5aa5e092abb0e96bcf6c4f3f7e85d5324.

--- Assets/Scripts/GhostControls/GhostControl.cs
+++ Assets/Scripts/GhostControls/GhostControl.cs
@@ -12,7 +12,6 @@ public class GhostControl : MonoBehaviour {
 	public float howLong = 1.0f;
 	public float howFast = 8.0f;
 
-
 	//determine the update frequency 
 	private float nextUpdate = 1.0f;
 
---
+++ Assets/Scripts/PacMan/LifeCounter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeCounter : MonoBehaviour
+{
+
+	public Text lifeText;
+
+	void Update ()
+	{
+		
+	}
+}
+
