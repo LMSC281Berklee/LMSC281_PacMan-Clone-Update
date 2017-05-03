60ae4230e2c85c6084719417da395d1faf734abe
pkeerasuntonpong <pkeerasuntonpong@berklee.edu>
2017-05-03 19:50:26 -0400

    Continuous movement works.. barely.. there's still some little weird quirks

--- Assets/Scripts/PacMan/PlayerMove.cs
+++ Assets/Scripts/PacMan/PlayerMove.cs
@@ -8,27 +8,42 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
-	//allow for editable movement values
-	public float speed = 6.0f;
-	public float gravity = 20.0f;
+    //allow for editable movement values
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
 
-	//a position holder
-	private Vector3 moveDirection;
+    //a position holder
+    private Vector3 moveDirection;
 
-	//determine if the player is in collision state
-	private CollisionFlags flags;
+    //determine if the player is in collision state
+    private CollisionFlags flags;
 
-	//hold the collision state information
-	private bool grounded  = false;
+    //hold the collision state information
+    private bool grounded = false;
 
-	void FixedUpdate() {
-		if (grounded) {
-			// We are grounded, so recalculate movedirection directly from axes
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-		}
+    private bool isKeyPressed = false;
 
+    private float horizontalMovement = 0;
+    private float verticalMovement = 0;
+
+
+    void FixedUpdate() {
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.S))
+        {
+            isKeyPressed = true;
+
+            horizontalMovement = Input.GetAxis("Horizontal");
+            verticalMovement = Input.GetAxis("Vertical");
+            Debug.Log(horizontalMovement + verticalMovement);       
+        }
+        if (isKeyPressed || grounded)
+        {
+            // We are grounded, so recalculate movedirection directly from axes
+            moveDirection = new Vector3(horizontalMovement * speed * Time.deltaTime, 0, verticalMovement * speed * Time.deltaTime);
+            transform.Translate(moveDirection);
+        }
+        
 		// Apply gravity, to adjust for the jump functionality
 		moveDirection.y -= gravity * Time.deltaTime;
 
@@ -36,5 +51,7 @@ public class PlayerMove : MonoBehaviour {
 		CharacterController controller = GetComponent<CharacterController>();
 		flags = controller.Move(moveDirection * Time.deltaTime);
 		grounded = (flags & CollisionFlags.CollidedBelow) != 0;
+
+        
 	}
 }
