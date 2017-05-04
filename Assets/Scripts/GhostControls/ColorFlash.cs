8dd1f876ec1b2bb03ad5238bc53f239e8d819549
Augustus Rivera <azrivera@berklee.edu>
2017-05-04 14:01:41 -0400

    set up default material colors for enemies to change back to

--- Assets/Scripts/GhostControls/ColorFlash.cs
+++ Assets/Scripts/GhostControls/ColorFlash.cs
@@ -4,25 +4,55 @@ using UnityEngine;
 
 public class ColorFlash : MonoBehaviour {
 	//define starting color for ghosts
-	public Color ColorStart = Color.blue;
+	public static Color ColorStart = Color.blue;
 
 
 	//define ending color for lerp flash
-	public Color colorEnd = Color.white;
+	public static Color colorEnd = Color.white;
 
-	public float duration = 1.0F;
-	public Renderer rend;
+	public static float duration = 1.0F;
+	public Renderer rend1;
+	public Renderer rend2;
+	public Renderer rend3;
+	public Renderer rend4;
 
 
 	void Start() {
-		rend = GetComponent<Renderer>();
+		rend1 = GameObject.Find("Blinky").GetComponent<Renderer>();
+		rend2 = GameObject.Find("Inky").GetComponent<Renderer>();
+		rend3 = GameObject.Find("Pinky").GetComponent<Renderer>();
+		rend4 = GameObject.Find("Clyde").GetComponent<Renderer>();
+
 	}
 	void Update() {
-		
-		if (DestroyPellet.superPowered == true && DestroyPellet.counter < 20)
-		{
-			float lerp = Mathf.PingPong (Time.time, duration) / duration;
-			rend.material.color = Color.Lerp (ColorStart, colorEnd, lerp);
-			}
+
+	rend1.material.color = new Color(255,0,28,191);
+
+	rend2.material.color = new Color(69,231,255,199);
+
+	rend3.material.color = new Color(255,126,212,225);
+
+	rend4.material.color = new Color(186,145,62,213);
+
+		if (DestroyPellet.counter > 0) {
+			
+			if (DestroyPellet.superPowered == true && DestroyPellet.counter < 20) {
+				float lerp = Mathf.PingPong (Time.time, duration) / duration;
+				rend1.material.color = Color.Lerp (ColorStart, colorEnd, lerp);
+
+				rend2.material.color = Color.Lerp (ColorStart, colorEnd, lerp);
+
+				rend3.material.color = Color.Lerp (ColorStart, colorEnd, lerp);
+
+				rend4.material.color = Color.Lerp (ColorStart, colorEnd, lerp);
+
+			
+			
+		}
+
+
+		}
 	}
+
+	
 }
